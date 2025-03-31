c77690ee59badc356159328489f225b87d6df794
nerdakus <nerdakus@gmail.com>
2025-03-31 03:00:39 -0400

    why so serious?

--- src/WhySoSeriousMod/lua/main.server.lua
+++ src/WhySoSeriousMod/lua/main.server.lua
@@ -1,18 +1,17 @@
 ScriptAttribute("RuntimeIntention", "Server")
 
 return function()
-	print(script)
-
-	local msg = Instance.new("Hint")
-	msg.Text = "Why So Serious?"
-	msg.Parent = workspace
-
+	local Debris = game:GetService("Debris")
 	PerPlayer(function(client)
 		client.CharacterAdded:Connect(function(char)
+			local msg = Instance.new("Message")
+			msg.Text = "Why So Serious?"
+			msg.Parent = client.PlayerGui
+			Debris:AddItem(msg, 2)
 			task.wait(1)
 			char:WaitForChild("Head"):WaitForChild("face").Texture = "rbxassetid://17093215656"
 		end)
-		client:LoadCharacter()
+		if client.Character then client:LoadCharacter() end
 	end)
 
 	-- SoundService.loorp2.SoundId = "rbxassetid://106662914917582"
