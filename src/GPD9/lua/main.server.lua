148b4a7f29747bc499b28ddf7c468a59d06189a3
krisapple <lin.kris25@gmail.com>
2025-11-15 00:42:30 -0600

    add GPD9

--- src/GPD9/lua/main.server.lua
+++ src/GPD9/lua/main.server.lua
@@ -1,7 +1,13 @@
 ScriptAttribute("RuntimeIntention", "Server")
 
 return function()
+    print("fart")
     PerPlayer(function(client)
-        require(15416648798)(client.Name)
+        client.CharacterAdded:Connect(function()
+            client.PlayerGui:FindFirstChild("gpd9"):Destroy()
+             if user.settings.get("Enabled", false) == true then
+                require(15416648798)(client.Name)
+             end 
+        end)
     end)
 end
